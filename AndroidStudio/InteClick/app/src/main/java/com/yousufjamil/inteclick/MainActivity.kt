ee49e2dc997b18337599610b900114320334842b
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-11-04 22:28:24 +0300

    Added project

---
+++ AndroidStudio/InteClick/app/src/main/java/com/yousufjamil/inteclick/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.yousufjamil.inteclick
+
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+//        Handler().postDelayed({
+//            val homeIntent = Intent(this, WebViewActivity::class.java)
+//            startActivity(homeIntent)
+//            finish()
+//        }, 1000)
+        supportActionBar?.hide()
+
+        Handler().postDelayed(
+            {
+                val connectionManager: ConnectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+                val activeNetwork: NetworkInfo? = connectionManager.activeNetworkInfo
+                val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+                if (isConnected) {
+                    val finishLoadingIntent = Intent(this, WebViewActivity::class.java)
+                    startActivity(finishLoadingIntent)
+                    finish()
+                } else {
+                    val internetIssueIntent = Intent(this, InternetProblemActivity::class.java)
+                    startActivity(internetIssueIntent)
+                    finish()
+                }
+            }, 3000
+        )
+    }
+}
